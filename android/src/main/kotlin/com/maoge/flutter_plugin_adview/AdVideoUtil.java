6753d713b256a61570dcf77705b7cb8b1c2b825e
刘俊敏 <gzliujm@163.com>
2019-10-31 21:01:33 +0800

    adview

--- android/src/main/kotlin/com/maoge/flutter_plugin_adview/AdVideoUtil.java
+++ android/src/main/kotlin/com/maoge/flutter_plugin_adview/AdVideoUtil.java
@@ -68,6 +68,7 @@ public class AdVideoUtil {
             }
             @Override
             public void onAdPlayEnd(String s, Boolean aBoolean) {
+                System.out.println("---------------ccc-----------");
                 Log.e("test", "onAdPlayEnd");
                 AdVideoUtil.type = 0x222;
                 new MyThread().start();
@@ -86,12 +87,14 @@ public class AdVideoUtil {
             }
             @Override
             public void onAdClose(String s) {
+                System.out.println("---------------bbb-----------");
                 Log.e("test", "onAdClose");
                 AdVideoUtil.type = 0x400;
                 new MyThread().start();
             }
             @Override
             public void onAdReady(String s) {
+                System.out.println("---------------aaa-----------");
                 AdVideoUtil.type = 0x200;
                 new MyThread().start();
             }
